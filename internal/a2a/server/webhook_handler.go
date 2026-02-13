62452da101deb963bb6fa83e187c7c1c3150c7b1
YixiaoGong <1213489062@qq.com>
2026-02-13 17:02:02 +0800

    1

--- internal/a2a/server/webhook_handler.go
+++ internal/a2a/server/webhook_handler.go
@@ -50,7 +50,7 @@ func (h *WebhookHandler) HandleNotification(w http.ResponseWriter, r *http.Reque
 			if h.skillClient != nil {
 				h.skillClient.RegisterRemoteAgentSkills(*notif.AgentCard)
 			}
-			log.Printf("Webhook: updated agent %s", notif.AgentID)
+			// log.Printf("Webhook: updated agent %s", notif.AgentID)
 		}
 	case registry_protocol.NotificationTypeAgentDeregistered:
 		h.registrar.Remove(notif.AgentID)
@@ -58,7 +58,7 @@ func (h *WebhookHandler) HandleNotification(w http.ResponseWriter, r *http.Reque
 		if h.skillClient != nil {
 			h.skillClient.UnregisterRemoteAgentSkills(notif.AgentID)
 		}
-		log.Printf("Webhook: removed agent %s", notif.AgentID)
+		// log.Printf("Webhook: removed agent %s", notif.AgentID)
 	default:
 		log.Printf("Webhook: unknown notification type %s", notif.Type)
 	}
